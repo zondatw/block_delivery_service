ff98d8398ba32f2cb3e8e2e39870d235fa60de50
zonda yang <u226699@gmail.com>
2026-01-26 20:01:38 +0800

    Add listener service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use solana_client::pubsub_client::PubsubClient;
+use solana_sdk::pubkey::Pubkey;
+
+pub fn listen(program_id: Pubkey) {
+    let url = "ws://127.0.0.1:8900";
+
+    let (_client, receiver) =
+        PubsubClient::logs_subscribe(
+            url,
+            solana_client::rpc_config::RpcTransactionLogsFilter::Mentions(vec![
+                program_id.to_string(),
+            ]),
+            solana_client::rpc_config::RpcTransactionLogsConfig {
+                commitment: None,
+            },
+        )
+        .unwrap();
+
+    for msg in receiver {
+        println!("LOG EVENT: {:?}", msg.value.logs);
+        // TODO: parse OrderCreated
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let program_id = "YourProgramIdHere".parse().unwrap();
+    std::thread::spawn(move || {
+        listen(program_id);
+    });
+
+    println!("Listening Solana events...");
+    loop {}
 }
